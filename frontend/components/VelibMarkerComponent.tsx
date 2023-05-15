bc35de9350f544a89ebe0f73055750267450b5c3
WinteruOfficiel <Winteru@the-nwo.fun>
2023-05-15 11:46:39 +0200

    Bug fix : capacité 0

--- frontend/components/VelibMarkerComponent.tsx
+++ frontend/components/VelibMarkerComponent.tsx
@@ -9,7 +9,6 @@ import { relative } from "path";
 // volé ici : https://gist.github.com/nik-john/7213821efb3d8a90f50252ea4d9d8c1d#file-icon-js
 function SvgIcon({ perc, text, color }: { perc: number, text: string, color: string }): JSX.Element {
     return (
-
             <svg width="50px" height="50px" viewBox="0 0 42 42" className="donut" aria-labelledby="beers-title beers-desc" role="img">
                 <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" fill="white" role="presentation"></circle>
                 <circle className="donut-ring" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="#d2d3d4" strokeWidth="3" role="presentation"></circle>
@@ -38,7 +37,7 @@ const testIcon = L.divIcon({
 })
 
 function generateIcon(numbikesavailable: number, capacity: number): L.DivIcon {
-    const pourcentage = Math.round((numbikesavailable / capacity) * 100)
+    const pourcentage: number = capacity > 0 ? Math.round((numbikesavailable / capacity) * 100) : 0;
     const icon = L.divIcon({
         className: style.icon,
         iconSize: [50, 50],
@@ -58,12 +57,12 @@ export default function VelibMarker({ station }: { station: VelibStationStatus }
         <Marker key={station.stationcode} icon={generateIcon(station.numbikesavailable, station.capacity)} position={[station.coordonnees_geo.y, station.coordonnees_geo.x]}>
             <Popup>
                 <div className={style.popup}>
-                    <h2>Station : {station.name}</h2>
+                    <h2>{station.name}</h2>
                     <p><strong>Communes : </strong>{station.nom_arrondissement_communes}</p>
                     <p><strong>Capacité : </strong>{station.capacity}</p>
-                    <p><strong>Nombres de vélos disponible :</strong> {station.numbikesavailable} ({Math.round(station.numbikesavailable / station.capacity * 100)}%)</p>
+                    <p><strong>Nombres de vélos disponible :</strong> {station.numbikesavailable} ({Math.round((station.numbikesavailable / station.capacity) * 100) || 0}%)</p>
                     <p><strong>Répartition des vélos :</strong> {station.ebike} électriques, {station.mechanical} mécaniques</p>
-                    <p><strong>Nombre de places disponibles :</strong> {station.numdocksavailable} ({Math.round(station.numdocksavailable / station.capacity * 100)}%)</p>
+                    <p><strong>Nombre de places disponibles :</strong> {station.numdocksavailable} ({Math.round((station.numdocksavailable / station.capacity) * 100) || 0}%)</p>
                 </div>
             </Popup>
         </Marker>
